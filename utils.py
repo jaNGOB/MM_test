a1d8f5357f5e61d12dad55d02f3fc06be4ffbbfa
jaNGOB <gobeli@hotmail.ch>
2018-09-14 15:33:23 +0900

    Add files via upload

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def dif(bid, ask):
     return (100 / ask * bid) - 100
 
 
-def compare(dt, count, bfx, polo):
+def compare(dt, count, bfx, polo, verbose=False):
     bfxBid = bfx['ETHBTC']['bids']['price']
     bfxAsk = bfx['ETHBTC']['asks']['price']
 
@@ -18,9 +18,9 @@ def compare(dt, count, bfx, polo):
         print(bfxBid, poloAsk)
         f = open('trades.txt', 'a+')
         f.write(dt + '\n')
-        f.write('Buy Bitfinex <-> Sell Poloniex' + '\n')
-        f.write(bfxBid + ' <-> ' + poloAsk + '\n')
-        f.write('Profit: ' + dif(bfxBid, poloAsk) + '\n')
+        f.write('Buy Bitfinex <-> Sell Poloniex \n')
+        f.write('{} <-> {} \n'.format(bfxBid, poloAsk))
+        f.write('Profit: {} \n'.format(dif(bfxBid, poloAsk)))
         f.close()
 
     elif poloBid > bfxAsk and (dif(poloBid, bfxAsk) > 0.5):
@@ -28,17 +28,17 @@ def compare(dt, count, bfx, polo):
         print(poloBid, bfxAsk)
         f = open('trades.txt', 'a+')
         f.write(dt + '\n')
-        f.write('Buy Poloniex <-> Sell Bitfinex' + '\n')
-        f.write(poloBid + ' <-> ' + bfxAsk + '\n')
-        f.write('Profit: ' + dif(bfxBid, poloAsk) + '\n')
+        f.write('Buy Poloniex <-> Sell Bitfinex \n')
+        f.write('{} <-> {} \n'.format(poloBid, bfxAsk))
+        f.write('Profit: {} \n'.format(dif(bfxBid, poloAsk)))
         f.close()
 
     else:
         print(dt, 'no opportunity found', count)
-        # print('PoloAsk: {} PoloBid: {}'.format(poloAsk, poloBid))
-        # print(dif(poloBid, bfxAsk))
-        # print('BfxAsk : {} BfxBid : {}'.format(bfxAsk, bfxBid))
-        # print(dif(bfxBid, poloAsk))
+        # print('PoloBid: {} BfxAsk : {}'.format(poloBid, bfxAsk))
+        # print('Dif. not big enough: {}'.format(dif(poloBid, bfxAsk)))
+        # print('BfxBid : {} PoloAsk: {}'.format(bfxBid, poloAsk))
+        # print('Dif. not big enough: {}'.format(dif(bfxBid, poloAsk)))
 
 
 def getBalances():
